2c5a8067b55e3be587c4ef5c64d3a3550207d49d
DaveJhaesonAlivio <97799276+DaveJhaesonAlivio@users.noreply.github.com>
2024-02-21 22:59:56 +0800

    edited the resposiveness of footer

--- SpotlessSolutions.Web/ClientApp/src/pages/dashboard/components/Footer/Footer.tsx
+++ SpotlessSolutions.Web/ClientApp/src/pages/dashboard/components/Footer/Footer.tsx
@@ -4,13 +4,13 @@ import { FaFacebookSquare } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bottom-0 w-screen font-sans">
-      <div className="bg-[#05132F] text-white p-10 flex flex-col md:flex-row justify-between">
+    <footer className="w-screen font-sans">
+      <div className="bg-[#05132F] text-white p-4 md:p-10 flex flex-col md:flex-row justify-between">
         <div className="flex items-center mb-4 md:mb-0">
           <FiPhone size={35} className="mr-4" />
           <div>
             <p className="text-xs">Call To Find Out More</p>
-            <p className="text-lg font-bold">0917 129 2231</p>
+            <p className="sm:text-sm lg:text-lg font-bold">0917 129 2231</p>
           </div>
         </div>
 
@@ -22,7 +22,7 @@ function Footer() {
               href="https://www.google.com/maps/place/TopDown+Cleaning+Services/@10.7339092,122.5190795,17z/data=!4m14!1m7!3m6!1s0x33aefb2aa70edb59:0xca69faa1cdc2d92e!2sTopDown+Cleaning+Services!8m2!3d10.7339092!4d122.5216544!16s%2Fg%2F11shlk1sys!3m5!1s0x33aefb2aa70edb59:0xca69faa1cdc2d92e!8m2!3d10.7339092!4d122.5216544!16s%2Fg%2F11shlk1sys?entry=ttu"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-lg font-bold cursor-pointer"
+              className="sm:text-sm lg:text-lg font-bold cursor-pointer"
             >
               Iloilo City, Philippines
             </a>
@@ -39,7 +39,7 @@ function Footer() {
             <FaFacebookSquare size={35} className="mr-4" />
             <div>
               <p className="text-xs">Visit Our FB Page For More</p>
-              <p className="text-lg font-bold">Facebook</p>
+              <p className="sm:text-sm lg:text-lg font-bold">Facebook</p>
             </div>
           </a>
         </div>
