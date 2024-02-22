d48858783a6f487074a717458ed4c7b3d49fa4e6
DaveJhaesonAlivio <97799276+DaveJhaesonAlivio@users.noreply.github.com>
2024-02-22 21:18:59 +0800

    responsive navbar

---
+++ SpotlessSolutions.Web/ClientApp/src/components/navigation/Navbar.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <>
+      <nav className="bg-[#05132F] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="src/assets/td_logo.jpg" className="h-8" alt="Flowbite Logo"/>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Topdown</span>
+          </a>
+          <div className={`flex ${menuOpen ? 'justify-start' : 'justify-end'} md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse`}>
+            <button type="button" className=" text-white focus:outline-none font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-[#EFA25D] dark:hover:bg-[#fc9439]">Log in</button>
+            <button type="button" onClick={toggleMenu} className={`md:hidden text-white focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-[#EFA25D] dark:hover:bg-[#fc9439] ${menuOpen ? 'order-1' : 'order-2'}`}>
+              {menuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </button>
+          </div>
+          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#05132F] dark:bg-gray-800 md:md:bg-[#05132F] dark:border-gray-700">
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-[#EFA25D] md:p-0 dark:text-white hover:bg-gray-700">Home</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-[#EFA25D] md:p-0 dark:text-white hover:bg-gray-700">About</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-[#EFA25D] md:p-0 dark:text-white hover:bg-gray-700">Serives</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-[#EFA25D] md:p-0 dark:text-white hover:bg-gray-700">FAQs</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
